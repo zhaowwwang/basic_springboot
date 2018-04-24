73cff4aa4a4145ddda460a17918b0ac3098c055a
王兆伟 <wangzhaowei@juzifenqi.com>
2018-04-24 10:44:47 +0800

    init repository

---
+++ basic-admin/src/main/java/com/basic/core/BasicAdminApplication.java
@@ -0,0 +1,12 @@
+package com.basic.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAdminApplication.class, args);
+	}
+}
---
+++ basic-common/src/main/java/wang/basic/boot/App.java
@@ -0,0 +1,13 @@
+package wang.basic.boot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
